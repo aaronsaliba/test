691ef5488444b185b4bc6af704dbb4094b11f1e1
Aaron Saliba <aaron.saliba@dqr-group.com>
2018-11-13 13:44:53 +0100

    abc

--- test.py
+++ test.py
@@ -3,16 +3,22 @@ import openpyxl.styles as xlst
 import requests as req
 import datetime as dt
 
-
-
 class vars:
     def __init__(self, work_book, work_sheet, cell):
+
+        """[3 essencial variables for populating a cell]
+        
+        Arguments:
+            work_book {[string]} -- [the name of the workbook]
+            work_sheet {[string]} -- [the name of the worksheet]
+            cell {[string]} -- [the name of the cell]
+        """
+
         self.work_book = work_book
         self.work_sheet = work_sheet
         self.cell = cell
     def active_testing_sheet(self):
         wb = xl.load_workbook(self.work_book)
-        ws = wb[self.work_sheet]
         wb.save(self.work_book)
     def today_in_cell(self):
         wb = xl.load_workbook(self.work_book)
