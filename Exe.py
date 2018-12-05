02bac0340e73c9fd1d5462cfe68688c4a9a5bab1
Aaron Saliba <aaron.saliba@dqr-group.com>
2018-12-05 08:16:58 +0100

    asdfasd

--- Exe.py
+++ Exe.py
@@ -6,114 +6,20 @@ import json
 import excel_writer_reader as ex
 import coinmarketcap_data_parser as cmc
 
-a1 = ex.cell_reader("testing.xlsx","Table","A4")
-b1,c1 = a1.split("(")
-d1 = c1.replace(")","")
-e1 = cmc.cmc_price(d1,"EUR")
-f1 = cmc.cmc_7d(d1,"EUR")/100
-ex.string_in_cell("testing.xlsx","Table","B4", e1)
-ex.string_in_cell("testing.xlsx","Table","C4", f1)
+# Timestamping
+ex.today_in_cell("testing.xlsx", "Table", "G4")
 
-a2 = ex.cell_reader("testing.xlsx","Table","A5")
-b2,c2 = a2.split("(")
-d2 = c2.replace(")","")
-e2 = cmc.cmc_price(d2,"EUR")
-f2 = cmc.cmc_7d(d2,"EUR")/100
-ex.string_in_cell("testing.xlsx","Table","B5", e2)
-ex.string_in_cell("testing.xlsx","Table","C5", f2)
+# Filling in data in top-part
+reading_set = ["A4", "A5", "A6", "A7", "A8", "A9", "A10", "A11", "A12", "A13", "A14", "A15", "A16", "A17"]
+printing_set1 = ["B4", "B5", "B6", "B7", "B8", "B9", "B10", "B11", "B12", "B13", "B14", "B15", "B16", "B17"]
+printing_set2 = ["C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13", "C14", "C15", "C16", "C17"]
 
-a3 = ex.cell_reader("testing.xlsx","Table","A6")
-b3,c3 = a3.split("(")
-d3 = c3.replace(")","")
-e3 = cmc.cmc_price(d3 ,"EUR")
-f3 = cmc.cmc_7d(d3,"EUR")/100
-ex.string_in_cell("testing.xlsx","Table","B6", e3)
-ex.string_in_cell("testing.xlsx","Table","C6", f3)
+for x, y, z in zip(reading_set, printing_set1, printing_set2):
+    a = ex.cell_reader("testing.xlsx", "Table", x)
+    b,c = a.split("(")
+    d = c.replace(")","")
+    e = cmc.cmc_price(d,"EUR")
+    f = cmc.cmc_7d(d,"EUR")/100
+    ex.string_in_cell("testing.xlsx","Table", y, e)
+    ex.string_in_cell("testing.xlsx","Table", z, f)
 
-a4 = ex.cell_reader("testing.xlsx","Table","A7")
-b4,c4 = a4.split("(")
-d4 = c4.replace(")","")
-e4 = cmc.cmc_price(d4 ,"EUR")
-f4 = cmc.cmc_7d(d4,"EUR")/100
-ex.string_in_cell("testing.xlsx","Table","B7", e4)
-ex.string_in_cell("testing.xlsx","Table","C7", f4)
-
-a5 = ex.cell_reader("testing.xlsx","Table","A8")
-b5,c5 = a5.split("(")
-d5 = c5.replace(")","")
-e5 = cmc.cmc_price(d5 ,"EUR")
-f5 = cmc.cmc_7d(d5,"EUR")/100
-ex.string_in_cell("testing.xlsx","Table","B8", e5)
-ex.string_in_cell("testing.xlsx","Table","C8", f5)
-
-a6 = ex.cell_reader("testing.xlsx","Table","A9")
-b6,c6 = a6.split("(")
-d6 = c6.replace(")","")
-e6 = cmc.cmc_price(d6 ,"EUR")
-f6 = cmc.cmc_7d(d6,"EUR")/100
-ex.string_in_cell("testing.xlsx","Table","B9", e6)
-ex.string_in_cell("testing.xlsx","Table","C9", f6)
-
-a7 = ex.cell_reader("testing.xlsx","Table","A10")
-b7,c7 = a7.split("(")
-d7 = c7.replace(")","")
-e7 = cmc.cmc_price(d7 ,"EUR")
-f7 = cmc.cmc_7d(d7,"EUR")/100
-ex.string_in_cell("testing.xlsx","Table","B10", e7)
-ex.string_in_cell("testing.xlsx","Table","C10", f7)
-
-a8 = ex.cell_reader("testing.xlsx","Table","A11")
-b8,c8 = a8.split("(")
-d8 = c8.replace(")","")
-e8 = cmc.cmc_price(d8 ,"EUR")
-f8 = cmc.cmc_7d(d8,"EUR")/100
-ex.string_in_cell("testing.xlsx","Table","B11", e8)
-ex.string_in_cell("testing.xlsx","Table","C11", f8)
-
-a9 = ex.cell_reader("testing.xlsx","Table","A12")
-b9,c9 = a9.split("(")
-d9 = c9.replace(")","")
-e9 = cmc.cmc_price(d9 ,"EUR")
-f9 = cmc.cmc_7d(d9,"EUR")/100
-ex.string_in_cell("testing.xlsx","Table","B12", e9)
-ex.string_in_cell("testing.xlsx","Table","C12", f9)
-
-a10 = ex.cell_reader("testing.xlsx","Table","A13")
-b10,c10 = a10.split("(")
-d10 = c10.replace(")","")
-e10 = cmc.cmc_price(d10 ,"EUR")
-f10 = cmc.cmc_7d(d10,"EUR")/100
-ex.string_in_cell("testing.xlsx","Table","B13", e10)
-ex.string_in_cell("testing.xlsx","Table","C13", f10)
-
-a11 = ex.cell_reader("testing.xlsx","Table","A14")
-b11,c11 = a11.split("(")
-d11 = c11.replace(")","")
-e11 = cmc.cmc_price(d11 ,"EUR")
-f11 = cmc.cmc_7d(d11,"EUR")/100
-ex.string_in_cell("testing.xlsx","Table","B14", e11)
-ex.string_in_cell("testing.xlsx","Table","C14", f11)
-
-a12 = ex.cell_reader("testing.xlsx","Table","A15")
-b12,c12 = a12.split("(")
-d12 = c12.replace(")","")
-e12 = cmc.cmc_price(d12 ,"EUR")
-f12 = cmc.cmc_7d(d12,"EUR")/100
-ex.string_in_cell("testing.xlsx","Table","B15", e12)
-ex.string_in_cell("testing.xlsx","Table","C15", f12)
-
-a13 = ex.cell_reader("testing.xlsx","Table","A16")
-b13,c13 = a13.split("(")
-d13 = c13.replace(")","")
-e13 = cmc.cmc_price(d13 ,"EUR")
-f13 = cmc.cmc_7d(d13,"EUR")/100
-ex.string_in_cell("testing.xlsx","Table","B16", e13)
-ex.string_in_cell("testing.xlsx","Table","C16", f13)
-
-a14 = ex.cell_reader("testing.xlsx","Table","A17")
-b14,c14 = a14.split("(")
-d14 = c14.replace(")","")
-e14 = cmc.cmc_price(d14 ,"EUR")
-f14 = cmc.cmc_7d(d14,"EUR")/100
-ex.string_in_cell("testing.xlsx","Table","B17", e14)
-ex.string_in_cell("testing.xlsx","Table","C17", f14)
--- excel_writer_reader.py
+++ excel_writer_reader.py
@@ -2,6 +2,8 @@ import openpyxl as xl
 import openpyxl.styles as xlst
 import requests as req
 import datetime as dt
+import pandas as pd
+import bs4
 
 def today_in_cell(workbook, sheet, cell):
     # today_in_cell("testing.xlsx","Table","G4")
@@ -45,8 +47,31 @@ def cell_reader(workbook, sheet, cell):
         sheet {[string]} -- [name of excel sheet. ex. "abc"]
         cell {[string]} -- [cell reference. ex: "A1"]
     """
-
     wb = xl.load_workbook(filename=workbook)
     ws = wb[sheet]
     val = ws[cell].value
     return val
+
+def load_soy_data():
+        
+    """[loads the begining of the year data in excel]
+        
+    Arguments:
+        workbook {[string]} -- [relative path of excel sheet. ex. "abc.xlsx"]
+        sheet {[string]} -- [name of excel sheet. ex. "abc"]
+    """
+    todays_date = dt.datetime.today()
+    current_year = todays_date.year
+    url = f"https://coinmarketcap.com/historical/20131001/"
+    data =req.get(url).text
+    soup = bs4.BeautifulSoup(data,"lxml")
+    table = soup.find(id ="currencies-all" )
+    tbody = table.find('tbody')
+    allTrs = tbody.find_all('tr')
+    for tr in allTrs:
+        for td in tr.find('td'):
+            t = td
+
+
+    
+load_soy_data()
